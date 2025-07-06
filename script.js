e5f2a2fa1bd5fc5b91bca7550a6cfe20f0f993fc
Talison-Freitas <talisondefreitas@outlook.com>
2025-07-06 01:10:25 -0300

    Cria a seção 'Contato'

---
+++ script.js
@@ -0,0 +1,4 @@
+const buttonForm = document.querySelector(".botao");
+// Previne para que a página não recarregue ao clicar no botão.
+buttonForm.addEventListener("click", (event) => event.preventDefault());
+
