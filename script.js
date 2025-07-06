945b295343354b7ca8c2dd76e0f07cd654e57cdf
Talison-Freitas <talisondefreitas@outlook.com>
2025-07-06 01:48:11 -0300

    Ajuste na responsividade da página e inserção de comentários

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 const buttonForm = document.querySelector(".botao");
 // Previne para que a página não recarregue ao clicar no botão.
 buttonForm.addEventListener("click", (event) => event.preventDefault());
-
